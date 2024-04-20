e67014b81538024d9ab692bf2350c806ba4cca94
AitorGallardo <38296940+AitorGallardo@users.noreply.github.com>
2024-04-20 23:29:06 +0200

    schema and migrations, no longer sst

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter as FontSans } from 'next/font/google'
 import '../styles/globals.css'
 
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/src/lib/utils"
 
 // const inter = Inter({ subsets: ['latin'] })
 
