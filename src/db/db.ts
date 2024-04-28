e50aa07c79c31e333997025721b587b8bb540273
AitorGallardo <38296940+AitorGallardo@users.noreply.github.com>
2024-04-28 21:25:54 +0200

    seeder working

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,6 +1,9 @@
 import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
 
+import 'dotenv/config';
+
+
 import * as schema from './schema'
 
 
--- src/db/seed.ts
+++ src/db/seed.ts
@@ -1,36 +1,63 @@
-import {db} from "@/src/db/db";
+import { db } from "@/src/db/db";
 import { bookmarks, users } from "./schema";
 
 const usersData = [
+  {
+    name: "John Doe",
+    email: "john@example.com",
+  },
+  {
+    name: "Jane Doe",
+    email: "jane@example.com",
+  },
+  {
+    name: "Bob Smith",
+    email: "bob@example.com",
+  },
+  {
+    name: "Alice Johnson",
+    email: "alice@example.com",
+  },
+];
+const bookmarksData = [
     {
-      name: 'John Doe',
-      email: 'john@example.com',
+      url: 'https://example1.com',
+      title: 'Example 1',
+      description: 'This is an example bookmark 1.',
+      icon: 'https://example1.com/icon.png',
     },
     {
-      name: 'Jane Doe',
-      email: 'jane@example.com',
+      url: 'https://example2.com',
+      title: 'Example 2',
+      description: 'This is an example bookmark 2.',
+      icon: 'https://example2.com/icon.png',
     },
     {
-      name: 'Bob Smith',
-      email: 'bob@example.com',
+      url: 'https://example3.com',
+      title: 'Example 3',
+      description: 'This is an example bookmark 3.',
+      icon: 'https://example3.com/icon.png',
     },
     {
-      name: 'Alice Johnson',
-      email: 'alice@example.com',
+      url: 'https://example4.com',
+      title: 'Example 4',
+      description: 'This is an example bookmark 4.',
+      icon: 'https://example4.com/icon.png',
     },
   ];
 
 const main = async () => {
-    try{
-        console.log('Seeding DB',);
-        // Delete all data
-        await db.delete(users);
-        await db.delete(bookmarks);
+  try {
+    console.log("Seeding DB");
+    // Delete all data
+    await db.delete(users);
+    await db.delete(bookmarks);
 
-        await db.insert(users).values(usersData)
+    await db.insert(users).values(usersData);
+    await db.insert(bookmarks).values(bookmarksData);
+  } catch {
+    console.log("error");
+  }
+};
 
-
-    }catch{
-        console.log('error')
-    }
-}
+main();
