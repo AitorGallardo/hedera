99e6d398b64e7703d4a9f79d2ae59c1b4adbd9e2
AitorGallardo <38296940+AitorGallardo@users.noreply.github.com>
2024-07-21 23:40:02 +0200

    working on diff cards info

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,28 @@ import GridItem from "../components/gridItem";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-slate-800">
-      <CommandDialogDemo />
-      <UploadButton />
+      {/* <CommandDialogDemo /> */}
+      {/* <UploadButton /> */}
       <section className="flex min-h-full w-full flex-col items-center justify-center p-24 space-y-4 bg-gray-500">
-        <div className="relative bg-white p-4"> 
-        <GridItem className="absolute bg-blue-400 origin-center rotate-[24deg]" />
-        <GridItem className="absolute bg-green-400 origin-center rotate-12" />
-        <GridItem className="absolute bg-yellow-400 origin-center rotate-6" />
-        <GridItem className="absolute bg-red-300 origin-center " />
+        <div className="relative bg-white p-4 w-36 h-36"> 
+        <GridItem className="absolute bg-gray-300 origin-center rotate-[24deg]" />
+        <GridItem className="absolute bg-gray-300 origin-center rotate-12" />
+        <GridItem className="absolute bg-gray-300 origin-center rotate-6" />
+        <GridItem className="absolute bg-gray-300 origin-center " />
         </div>
+
       </section>
+
+
+
+      <div className="grid grid-cols-6 gap-4 w-full bg-green-300">
+          <GridItem className="bg-gray-300 w-[200px] h-[150px]" />
+          <GridItem className="bg-gray-300 w-[200px] h-[150px]" />
+          <GridItem className="bg-gray-300 w-[200px] h-[150px]" />
+          <GridItem className="bg-gray-300 w-[200px] h-[150px]" />
+          <GridItem className="bg-gray-300 w-[200px] h-[150px]" />
+          <GridItem className="bg-gray-300 w-[200px] h-[150px]" />
+        </div>
     </main>
   );
 }
--- src/components/gridItem.tsx
+++ src/components/gridItem.tsx
@@ -2,14 +2,24 @@
 
 import { cn } from "../lib/utils";
 
+const IMAGE_URL = "https://cdn.dribbble.com/userupload/12164631/file/original-dc161c06881cbd184b94bb32526b2c88.png?resize=1024x810";
+
 interface CardProps {
   children?: React.ReactNode;
 }
 
-const GridItem = ({ children, className,height = 200, width = 200, ...props }:any) => {
+const GridItem = ({ image=IMAGE_URL, title, tags=['styles','tailwind','animations'], className, height = 200, width = 200, ...props }: any) => {
   return (
-    <div className={cn(`absolute h-[${height}px] w-[${width}px] rounded-sm`,className)}> 
-      {children}
+    <div className={cn(`absolute h-[${height}px] w-[${width}px] rounded-sm`, className)} {...props}>
+      <img src={image} alt={title} className="h-full w-full object-cover rounded-sm" />
+      <p className="text-sm font-bold">{title}</p>
+      <div className="flex flex-wrap">
+        {tags.map((tag: string, index: number) => (
+          <span key={index} className="text-xs m-1 bg-gray-200 rounded-full px-2 py-1">
+            {tag}
+          </span>
+        ))}
+      </div>
     </div>
   );
 };
